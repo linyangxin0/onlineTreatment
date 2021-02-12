a0af938940bbd340a8da13d327e86860d9e7c078
linyangxin <1227143051@qq.com>
2021-02-12 23:41:23 +0800

    增加主页背景图片

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,63 +1,66 @@
 <template>
-  <div class="home">
-    <div class="home-left">
-      <div class="home-left-top">
-        <div><span class="home-left-top-title">个人信息：</span></div>
-        <div><span class="home-left-top-info">用户名：{{ userName }}</span></div>
-        <div><span class="home-left-top-info">账 户：{{ account }}</span></div>
-        <div class="home-left-top-logout-btn">
-          <el-button size="small" @click="changePassword" type="info">修改密码</el-button>
-          <el-button size="small" @click="logout" type="danger">退出登录</el-button>
+  <div id="home">
+    <div class="home-content">
+      <div class="home-left">
+        <div class="home-left-top">
+          <div><span class="home-left-top-title">个人信息：</span></div>
+          <div><span class="home-left-top-info">用户名：{{ userName }}</span></div>
+          <div><span class="home-left-top-info">账 户：{{ account }}</span></div>
+          <div class="home-left-top-logout-btn">
+            <el-button size="small" @click="changePassword" type="info">修改密码</el-button>
+            <el-button size="small" @click="logout" type="danger">退出登录</el-button>
+          </div>
         </div>
-      </div>
 
-      <div class="home-left-bottom">
-        <div class="home-left-bottom-top">
-          <span>当前在线人数：</span>
-        </div>
-        <div class="home-left-bottom-center">
+        <div class="home-left-bottom">
+          <div class="home-left-bottom-top">
+            <span>当前在线人数：</span>
+          </div>
+          <div class="home-left-bottom-center">
           <span>
             {{ onlineNum }}
           </span>
-        </div>
-        <div class="home-left-bottom-bottom">
-          <el-input placeholder="请输入房间名" v-model="roomName">
-            <el-button slot="append" icon="el-icon-s-home" @click="createRoom">创建房间</el-button>
-          </el-input>
+          </div>
+          <div class="home-left-bottom-bottom">
+            <el-input placeholder="请输入房间名" v-model="roomName">
+              <el-button slot="append" icon="el-icon-s-home" @click="createRoom">创建房间</el-button>
+            </el-input>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="home-center">
-      <div class="home-center-title">
-        <span>当前在线</span>
-      </div>
-      <div class="home-center-content">
-        <div v-for="item in userList" class="home-center-content-list">
-          {{ item }}
+      <div class="home-center">
+        <div class="home-center-title">
+          <span>当前在线</span>
+        </div>
+        <div class="home-center-content">
+          <div v-for="item in userList" class="home-center-content-list">
+            {{ item }}
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="home-right">
-      <div class="home-right-title">
+      <div class="home-right">
+        <div class="home-right-title">
         <span>
           房间列表
         </span>
-      </div>
-      <div class="home-right-bottom">
-        <div v-for="item in rooms" class="home-right-content">
-          <div class="home-right-content-room-name">
-            <span>{{ item.roomName }}</span>
-          </div>
-          <div class="home-right-content-enter-btn">
-            <el-button @click="enterRoom(item)" size="mini">进入房间</el-button>
+        </div>
+        <div class="home-right-bottom">
+          <div v-for="item in rooms" class="home-right-content">
+            <div class="home-right-content-room-name">
+              <span>{{ item.roomName }}</span>
+            </div>
+            <div class="home-right-content-enter-btn">
+              <el-button @click="enterRoom(item)" size="mini">进入房间</el-button>
+            </div>
           </div>
         </div>
-      </div>
 
+      </div>
     </div>
   </div>
+
 </template>
 
 <script>
@@ -140,7 +143,17 @@ export default {
   display: none; /* Chrome Safari */
 }
 
-.home {
+#home {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+
+  background-image: url("../assets/img/homeBg.jpg");
+}
+
+.home-content {
   position: absolute;
   left: 0;
   right: 0;
@@ -148,6 +161,7 @@ export default {
   bottom: 0;
   margin: 20px;
   display: flex;
+  opacity: 95%;
 }
 
 .home-left {
@@ -160,7 +174,9 @@ export default {
   flex: 1;
   margin: 30px;
   padding: 30px;
-  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+  border-radius: 10px;
 }
 
 .home-left-top-title {
@@ -182,6 +198,8 @@ export default {
   padding: 30px;
   box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
   position: relative;
+  background-color: #fff;
+  border-radius: 10px;
 }
 
 .home-left-bottom-top {
@@ -206,6 +224,9 @@ export default {
   margin: 30px 0;
   box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
   position: relative;
+
+  background-color: #fff;
+  border-radius: 10px;
 }
 
 .home-center-title {
@@ -232,13 +253,15 @@ export default {
   border-bottom: 1px solid #DCDCDC;
 }
 
-
 .home-right {
   flex: 1;
   margin: 30px;
   padding: 30px;
   box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
   position: relative;
+
+  background-color: #fff;
+  border-radius: 10px;
 }
 
 .home-right-title {

796a2141a73a087b0438e14239614a9ac3d9bd5d
linyangxin <1227143051@qq.com>
2021-02-13 09:03:40 +0800

    增加注释

--- serve/dao/UserDao.js
+++ serve/dao/UserDao.js
@@ -9,6 +9,7 @@ let connection = mysql.createConnection({
 
 connection.connect();
 
+//修改密码
 module.exports.updateUserInPassword = function (account, newPassword, callback) {
     return connection.query('update user set password=? where account=?',
         [newPassword, account], function (error, results, fields) {
@@ -17,6 +18,7 @@ module.exports.updateUserInPassword = function (account, newPassword, callback) 
         })
 }
 
+//注册
 module.exports.insertUser = function (userName, account, password, callback) {
     return connection.query('insert into user (name,account,password) values(?,?,?)',
         [userName, account, password], function (error, results, fields) {
@@ -25,6 +27,7 @@ module.exports.insertUser = function (userName, account, password, callback) {
         })
 }
 
+//根据账号查找user
 module.exports.selectUserByAccount = function (account, callback) {
     return connection.query('select * from user where account = ?',
         [account], function (error, results, fields) {
@@ -33,6 +36,7 @@ module.exports.selectUserByAccount = function (account, callback) {
         })
 }
 
+//根据账号密码查询user
 module.exports.selectUserByAccountAndPassword = function (account, password, callback) {
     return connection.query('select * from user where account = ? and password = ?',
         [account, password], function (error, results, fields) {
